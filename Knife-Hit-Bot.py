da073e1d8b833de024011fe19276aa1ff52bfed1
Daniel <satelerd@gmail.com>
2021-07-06 20:47:48 -0400

    first working game

---
+++ Knife-Hit-Bot.py
@@ -0,0 +1,71 @@
+import cv2
+import pyautogui
+import time
+
+# Uploading images
+not_knife_img2 = cv2.imread(r'C:\Users\satel\OneDrive\Escritorio\Code\Ketchupp-Bot\img\not_knife2.png', cv2.COLOR_BGR2GRAY)
+not_knife_img3 = cv2.imread(r'C:\Users\satel\OneDrive\Escritorio\Code\Ketchupp-Bot\img\not_knife3.png', cv2.COLOR_BGR2GRAY)
+knife_img = cv2.imread(r'C:\Users\satel\OneDrive\Escritorio\Code\Ketchupp-Bot\img\nailed_knife.png', cv2.COLOR_BGR2GRAY)
+knife_45_img = cv2.imread(r'C:\Users\satel\OneDrive\Escritorio\Code\Ketchupp-Bot\img\nailed_knife_45.png', cv2.COLOR_BGR2GRAY)
+flip_knife_45_img = cv2.flip(knife_45_img, 1)
+
+searching1 = knife_img
+searching2 = knife_45_img # Img for search
+searching3 = flip_knife_45_img
+
+cont = 0
+# Game loop
+while True:
+    print("here we go again")
+
+    time1 = time.time()
+    # Take the screenshot and save it
+    ss = pyautogui.screenshot(region=(700,550,550,150))  #500,0,800,1000 #700,550,550,150 # Needs finetune
+    ss.save(r'C:\Users\satel\OneDrive\Escritorio\Code\Ketchupp-Bot\screenshots\ss1.png')
+    ss1 = cv2.imread(r'C:\Users\satel\OneDrive\Escritorio\Code\Ketchupp-Bot\screenshots\ss1.png', cv2.COLOR_BGR2GRAY)
+
+    # searching for the img
+    result1 = cv2.matchTemplate(ss1, searching1, cv2.TM_CCOEFF_NORMED)
+    min_val1, max_val1, min_loc1, max_loc1 = cv2.minMaxLoc(result1)
+    print(max_val1)
+
+    result2 = cv2.matchTemplate(ss1, searching2, cv2.TM_CCOEFF_NORMED)
+    min_val2, max_val2, min_loc2, max_loc2 = cv2.minMaxLoc(result2)
+    print(max_val2)
+
+    result3 = cv2.matchTemplate(ss1, searching3, cv2.TM_CCOEFF_NORMED)
+    min_val3, max_val3, min_loc3, max_loc3 = cv2.minMaxLoc(result3)
+    print(max_val3)
+
+    if max_val1 <= 0.7 and max_val2 <= 0.53 and max_val3 <=0.53:
+        pyautogui.click()
+        print("dispara locoooo")
+
+    else:
+        print("cuidadooooo voroooooo")
+    
+    time2 = time.time()
+    print(f"tiempo: {time2-time1}")
+    time.sleep(0.1)
+
+    # Creating the rectagle where found the img and display it
+    # w = searching1.shape[1]
+    # h = searching1.shape[0]
+    # cv2.rectangle(ss1, max_loc1, (max_loc1[0] + w, max_loc1[1] + h), (0,0,255), 2)
+    # w = searching2.shape[1]
+    # h = searching2.shape[0]
+    # cv2.rectangle(ss1, max_loc2, (max_loc2[0] + w, max_loc2[1] + h), (0,0,255), 2)
+    # w = searching3.shape[1]
+    # h = searching3.shape[0]
+    # cv2.rectangle(ss1, max_loc3, (max_loc3[0] + w, max_loc3[1] + h), (0,0,255), 2)
+
+    # cv2.imshow("img", ss1)
+    # cv2.waitKey()
+    # cv2.destroyAllWindows()
+
+
+    cont += 1
+
+    if cont == 1000:
+        break
+
