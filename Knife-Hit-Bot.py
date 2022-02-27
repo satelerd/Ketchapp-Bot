29ba28f297419ca19e2d2d793ab7e92af6899596
Daniel <satelerd@gmail.com>
2022-02-27 19:43:58 -0300

    path fix

--- Knife-Hit-Bot.py
+++ Knife-Hit-Bot.py
@@ -1,16 +1,34 @@
 import cv2
 import pyautogui
+import numpy as np
 import time
 
+# PATH to Ketchapp-Bot folder
+path = r"C:\Users\satel\OneDrive\code\Artilujios\Ketchapp-Bot"
+# print(f"{path}\img\not_knife2.png")
+
 # Uploading images
-not_knife_img2 = cv2.imread(r'C:\Users\satel\OneDrive\Escritorio\Code\Ketchupp-Bot\img\not_knife2.png', cv2.COLOR_BGR2GRAY)
-not_knife_img3 = cv2.imread(r'C:\Users\satel\OneDrive\Escritorio\Code\Ketchupp-Bot\img\not_knife3.png', cv2.COLOR_BGR2GRAY)
-knife_img = cv2.imread(r'C:\Users\satel\OneDrive\Escritorio\Code\Ketchupp-Bot\img\nailed_knife.png', cv2.COLOR_BGR2GRAY)
-knife_45_img = cv2.imread(r'C:\Users\satel\OneDrive\Escritorio\Code\Ketchupp-Bot\img\nailed_knife_45.png', cv2.COLOR_BGR2GRAY)
+not_knife_img2 = cv2.imread(
+    rf"{path}\img\not_knife2.png",
+    cv2.COLOR_BGR2GRAY,
+)
+not_knife_img3 = cv2.imread(
+    rf"{path}\img\not_knife3.png",
+    cv2.COLOR_BGR2GRAY,
+)
+knife_img = cv2.imread(
+    rf"{path}\img\nailed_knife.png",
+    cv2.COLOR_BGR2GRAY,
+)
+knife_45_img = cv2.imread(
+    rf"{path}\img\nailed_knife_45.png",
+    cv2.COLOR_BGR2GRAY,
+)
 flip_knife_45_img = cv2.flip(knife_45_img, 1)
 
+# Img for search
 searching1 = knife_img
-searching2 = knife_45_img # Img for search
+searching2 = knife_45_img
 searching3 = flip_knife_45_img
 
 cont = 0
@@ -20,12 +38,19 @@ while True:
 
     time1 = time.time()
     # Take the screenshot and save it
-    ss = pyautogui.screenshot(region=(700,550,550,150))  #500,0,800,1000 #700,550,550,150 # Needs finetune
-    ss.save(r'C:\Users\satel\OneDrive\Escritorio\Code\Ketchupp-Bot\screenshots\ss1.png')
-    ss1 = cv2.imread(r'C:\Users\satel\OneDrive\Escritorio\Code\Ketchupp-Bot\screenshots\ss1.png', cv2.COLOR_BGR2GRAY)
+    ss = pyautogui.screenshot(
+        region=(700, 550, 550, 150)
+    )  # UPDATE THIS TO YOUR SCREENSHOT LOCATION
+    ss.save(rf"{path}\screenshots\ss1.png")
+    ss1 = cv2.imread(
+        rf"{path}\screenshots\ss1.png",
+        cv2.COLOR_BGR2GRAY,
+    )
 
     # searching for the img
-    result1 = cv2.matchTemplate(ss1, searching1, cv2.TM_CCOEFF_NORMED)
+    result1 = cv2.matchTemplate(
+        ss1.astype(np.float32), searching1.astype(np.float32), cv2.TM_CCOEFF_NORMED
+    )
     min_val1, max_val1, min_loc1, max_loc1 = cv2.minMaxLoc(result1)
     print(max_val1)
 
@@ -37,33 +62,32 @@ while True:
     min_val3, max_val3, min_loc3, max_loc3 = cv2.minMaxLoc(result3)
     print(max_val3)
 
-    if max_val1 <= 0.7 and max_val2 <= 0.53 and max_val3 <=0.53:
+    if max_val1 <= 0.7 and max_val2 <= 0.53 and max_val3 <= 0.53:
         pyautogui.click()
         print("dispara locoooo")
 
     else:
         print("cuidadooooo voroooooo")
-    
+
     time2 = time.time()
     print(f"tiempo: {time2-time1}")
     time.sleep(0.1)
 
     # Creating the rectagle where found the img and display it
-    # w = searching1.shape[1]
-    # h = searching1.shape[0]
-    # cv2.rectangle(ss1, max_loc1, (max_loc1[0] + w, max_loc1[1] + h), (0,0,255), 2)
-    # w = searching2.shape[1]
-    # h = searching2.shape[0]
-    # cv2.rectangle(ss1, max_loc2, (max_loc2[0] + w, max_loc2[1] + h), (0,0,255), 2)
-    # w = searching3.shape[1]
-    # h = searching3.shape[0]
-    # cv2.rectangle(ss1, max_loc3, (max_loc3[0] + w, max_loc3[1] + h), (0,0,255), 2)
+    w = searching1.shape[1]
+    h = searching1.shape[0]
+    cv2.rectangle(ss1, max_loc1, (max_loc1[0] + w, max_loc1[1] + h), (0, 0, 255), 2)
+    w = searching2.shape[1]
+    h = searching2.shape[0]
+    cv2.rectangle(ss1, max_loc2, (max_loc2[0] + w, max_loc2[1] + h), (0, 0, 255), 2)
+    w = searching3.shape[1]
+    h = searching3.shape[0]
+    cv2.rectangle(ss1, max_loc3, (max_loc3[0] + w, max_loc3[1] + h), (0, 0, 255), 2)
 
-    # cv2.imshow("img", ss1)
-    # cv2.waitKey()
-    # cv2.destroyAllWindows()
+    cv2.imshow("img", ss1)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
 
     cont += 1
     if cont == 1000:
         break
-
